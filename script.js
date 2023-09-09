ecb5d5e59cf4ff5414d48e6e687a263f8ba0d987
jespertjarnfors <139288555+jespertjarnfors@users.noreply.github.com>
2023-09-09 22:47:06 +1200

    The functionality and error-handling is more or less complete!

--- script.js
+++ script.js
@@ -1,23 +1,118 @@
 
-let value;
+let buttonValue = '';
+let currentNumber = '';
+let displayValue = "";
+let operator = ''; 
+let result = 0;
+let isError = false;
+let clickedOperator = false;
+let clickedEquals = false;
+// declaring global variables.
 
 const allButtons = document.querySelectorAll('.buttons');
+const acButton = document.getElementById('ac-btn');
+// selecting all buttons with the .buttons class and declaring them under one variable.
 
-function displayValue (event) {
+function calculate() {
 
-    console.log('Button clicked');
+    if (currentNumber != 0) {
+        // ensuring all variables have a value.
 
-    value = event.target.value;
+      switch (operator) {
+        case '+':
+          result = parseFloat(currentNumber) + parseFloat(displayValue);
+          break;
+        case '-':
+          result = parseFloat(currentNumber) - parseFloat(displayValue);
+          break;
+        case '*':
+          result = parseFloat(currentNumber) * parseFloat(displayValue);
+          break;
+        case '/':
+          if (parseFloat(displayValue) === 0) {
+            // Minor error-handling if divided by 0.
+            displayValue = 'Error';
+            isError = true;
+            acButton.style.backgroundColor = '#D6605C';
+            return;
+          }
+          result = parseFloat(currentNumber) / parseFloat(displayValue);
+          break;
+        default:
+          break;
+      }
+        displayValue = result.toString().slice(0,9);
+        // returning a string and slicing out the initial 9-numbers in case a division causes a lot of decimals.
+    }
+    return;
+  }
+ 
 
-    document.getElementById('display-number').textContent = value;
+function retrieValue (event) {
 
-    console.log(value);
+    buttonValue = event.target.value;
+    // retrieve the value from button which is clicked.
 
+    if (buttonValue === 'ac') {
+        displayValue = "";
+        isError = false;
+        acButton.style.backgroundColor = '#EDF0FA';
+    }
+    // Linked to the error-handling below. Only the AC-button will return the isError state to false, and reset 'Error' in the window.
+
+    else if (isError) {
+        return;
+    }
+    // Prevents anything else from occupying the display until it's been reset by the AC-button.
+
+    else if (buttonValue === '=') {
+        calculate();
+        if (displayValue !='Error') {
+        document.getElementById('display-number').textContent = displayValue;
+        currentNumber = displayValue;
+        console.log(currentNumber);
+        displayValue = "";
+        clickedEquals = true;
+        return;
+    }
+   }
+
+    else if (!isNaN(buttonValue)) {
+
+        if (clickedOperator === true) {
+            displayValue = "";
+            displayValue += buttonValue;
+            clickedOperator = false;
+        } else {
+            displayValue += buttonValue;
+        }}
+        
+        else if (buttonValue === "+" || buttonValue === "-" || buttonValue === "/" || buttonValue === "*") {
+
+          if (clickedEquals === false) {
+            currentNumber = displayValue;
+            }
+            console.log(currentNumber);
+            displayValue = buttonValue;
+            operator = buttonValue;
+            clickedOperator = true;
+            clickedEquals = true;
+    }    
+
+    if (displayValue === 'Error' || displayValue.length >= 9) {
+    displayValue = "Error";
+    isError = true;
+    acButton.style.backgroundColor = '#D6605C';
+   }
+   // Error-handling, to make sure everything fits in the display window.
+
+
+   document.getElementById('display-number').textContent = displayValue;
+   // Finally, the display is updated.
 }
 
-
 allButtons.forEach(button => {
-    button.addEventListener('click', displayValue);
+    button.addEventListener('click', retrieValue);
 })
 
 
